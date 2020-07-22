d446e472d0ccf4f817b108314d14de3a91e15ff1
Ruslan Ponomarev <ruslan.ponomarev@wearebrain.com>
2020-07-22 18:08:04 +0300

    addRemoveFunc

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { moviesData} from "../moviesData";
+// import { render } from "react-dom";
+import MovieItem from "./MovieItem";
+
+class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            movies: moviesData
+
+        };
+
+        this.removeMovie =this.removeMovie.bind(this);
+    }
+    removeMovie = movie => {
+        const updateMovies = this.state.movies.filter(function(item) {
+            return item.id !== movie.id;
+        });
+        console.log(updateMovies);
+        // this.state.movies = updateMovies;
+        this.setState({
+            movies: updateMovies
+        });
+    }
+    render() {
+        console.log("render", this.state, this);
+        return (
+            <div>
+                {this.state.movies.map(movie => {
+                    return <MovieItem 
+                        key={movie.id} 
+                        movie={movie} 
+                        removeMovie={this.removeMovie}
+                    />
+                })}
+            </div>
+        );
+    }
+}
+
+export default App;
